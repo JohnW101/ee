a324fdd67acd6f5e5387bded7eee2b9d88ef2c5c
JohnW101 <36316425+JohnW101@users.noreply.github.com>
2020-05-03 01:43:09 -0400

    Create eek.lua

---
+++ eek.lua
@@ -0,0 +1,20 @@
+function autofarm()
+	for c,da in next, workspace.Alive:GetDescendants() do
+		if (da:FindFirstChildOfClass("Humanoid")) then
+			if da.Name == "Big Hoss" then 
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = da.HumanoidRootPart.CFrame + Vector3.new(0,9,0)
+			end
+		end
+	end
+end
+
+function nclip()
+	game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+end
+
+
+game:GetService"RunService".Stepped:connect(function()
+	autofarm();
+	rot();
+	nclip();
+end)
